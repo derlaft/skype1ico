05d1d16233856a7a638975ba4e3fcd1fadc73e9d
Alexey Derlaft <derlafff@ya.ru>
2014-05-08 00:21:41 +0400

    Theming support

--- skype1ico.cpp
+++ skype1ico.cpp
@@ -2,6 +2,7 @@
 
 setIconProto realSetIcon;
 QHash<qint32, QIcon> replace;
+QString theme_dir, unknown_dir;
 
 void initMyself() {
     realSetIcon = &QSystemTrayIcon::setIcon;
@@ -13,8 +14,42 @@ void initMyself() {
     memcpy(&realSetIcon, &ptr, sizeof(ptr));
 }
 
-void readConfig() {
-    QFile input_file("config");
+QString emptyDefault (QString query, QString def) {
+    if (query.isEmpty()) {
+        return def;
+    }
+    return query;
+}
+
+QString findThemeDir() {
+
+    QStringList themes, list;
+
+    themes << emptyDefault(qgetenv("SKYPE1ICO_THEME"), QIcon::themeName())
+           << "default";
+
+    list << qgetenv("HOME") + "/.local/share/skype1ico/"
+         << THEMES_DIR;
+
+    foreach (QString data_dir, QString(qgetenv("XDG_DATA_DIRS")).split(":")) {
+        list << data_dir + "/skype1ico/";
+    }
+
+    foreach(QString theme, themes) {
+        foreach (QString ico_root, list) {
+            QString path = QString("%1/%2/").arg(ico_root, theme);
+            printf("Checking " + path.toLatin1() + "\n");
+            if (QFile::exists( path + "themerc" )) {
+                return path;
+            }
+        }
+    }
+
+    return "";
+}
+
+void readConfig(QString theme_dir) {
+    QFile input_file(theme_dir + "themerc");
     if (input_file.open(QIODevice::ReadOnly))
     {
         QTextStream in(&input_file);
@@ -27,15 +62,16 @@ void readConfig() {
 
             if (line.endsWith(':')) {
                 line.chop(1);
-                value = QIcon(QString("replace/%1").arg( line ));
-
+                value = QIcon(theme_dir + line);
+                printf("Adding " + QString(theme_dir + line).toLatin1() + "\n");
+            }
+            else if (line.endsWith('!')) {
+                line.chop(1);
+                value = QIcon::fromTheme(line);
             }
             else if (!line.startsWith('#') && !value.isNull()) {
-
                 qint32 key = line.trimmed().toInt();
                 replace.insert(key, value);
-
-                //printf("lol %d\n", key);
             }
 
         }
@@ -46,14 +82,15 @@ void readConfig() {
 extern void QSystemTrayIcon::setIcon(const QIcon &icon) {
     if (realSetIcon == NULL) {
         initMyself();
-        readConfig();
+        theme_dir = findThemeDir();
+        readConfig(theme_dir);
     }
 
     QPixmap pixmap = icon.pixmap(QSize(24, 24));
     qint32 key = Hashing::hashPixmap(pixmap);
 
     if (!replace.contains(key)) {
-        pixmap.save(QString("source/%1.png").arg( key ));
+        pixmap.save(theme_dir + key + "-unknown.png");
     }
 
     (this->*realSetIcon) (replace.value(key, icon));
--- skype1ico.pro
+++ skype1ico.pro
@@ -1,3 +1,7 @@
+### eqmake4 was here ###
+CONFIG -= debug_and_release debug
+CONFIG += release
+
 #-------------------------------------------------
 #
 # Project created by QtCreator 2014-03-18T23:59:05
@@ -19,10 +23,17 @@ HEADERS += skype1ico.h\
     hashing.h
 
 unix {
-    target.path = /usr/lib
-    INSTALLS += target
+    isEmpty(PREFIX) {
+        PREFIX = /usr
+    }
+
+    target.path = $$PREFIX/lib
+
+    THEMES = $$PREFIX/share/skype1ico
+    themes.path = $$THEMES
+    themes.files += kfaenza
+
+    DEFINES += THEMES_DIR=\\\"$$THEMES/\\\"
+    DEFINES += DEFAULT_THEME=\\\"kfaenza\\\"
+    INSTALLS += target themes
 }
-
-QMAKE_CXXFLAGS += -m32
-LIBS = -L/usr/lib32 -L/usr/lib32/qt4 -L/usr/lib32 -L/usr/lib32/qt4
-
